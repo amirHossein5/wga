2f72f2fbc529945dd7cf491174199237222ec378
amirHossein5 <amir5665@yahoo.com>
2024-11-11 22:01:48 +0330

    feat: countdown with one player

--- main.go
+++ main.go
@@ -2,28 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"golang.org/x/net/websocket"
 )
 
-type Server struct {
-	conns []*websocket.Conn
+type Player struct {
+	num int
 }
 
-func NewServer() *Server {
-	return &Server{
-		conns: make([]*websocket.Conn, 0),
+var player Player = Player{num: 0}
+
+func updatePlayer(ws *websocket.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("failed to read websocket data", err)
+			continue
+		}
+
+		msg := string(buf[:n])
+
+		int, err := strconv.Atoi(msg)
+		if err != nil {
+			log.Println(msg, "is not a number")
+			continue
+		}
+
+		player.num += int
+
+		ws.Write([]byte(strconv.Itoa(player.num)))
 	}
-}
-
-func indexPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
 	http.HandleFunc("/", indexPage)
+	http.Handle("/update-player", websocket.Handler(updatePlayer))
 
 	port := 8080
 	log.Println("starting server in port", port)
@@ -31,4 +52,8 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to start server", err)
 	}
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
